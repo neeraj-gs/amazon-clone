86ed033274d4fc28c76d71e6b515dd8b361c6712
neeraj-gs <gsneeraj2002@gmail.com>
2023-08-21 12:25:46 +0530

    Made the Banner Masking fro top to bottom

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className='home'>
         <div className="home__container">
-            <img src={Banner} alt="Banner in Home Page" />
+            <img className='home__image' src={Banner} alt="Banner in Home Page" />
         </div>
     </div>
   )
