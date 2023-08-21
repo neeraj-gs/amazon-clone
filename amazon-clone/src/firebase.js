8404b0fc94570914202b61c3cfb4992e54c7255a
neeraj-gs <gsneeraj2002@gmail.com>
2023-08-21 09:42:08 +0530

    React frontend envirment ready

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC58EV7B2aPNhGv2RmZYaB6sd_0EAXeK-g",
+    authDomain: "clone-6cef7.firebaseapp.com",
+    projectId: "clone-6cef7",
+    storageBucket: "clone-6cef7.appspot.com",
+    messagingSenderId: "454344584442",
+    appId: "1:454344584442:web:56db0d89d03849901b2d43",
+    measurementId: "G-DXT3Z8GXLY"
+  };
