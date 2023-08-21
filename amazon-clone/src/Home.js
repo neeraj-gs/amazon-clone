b5fe8780c2277d0bcb5b72346fdb2303ecf3b961
neeraj-gs <gsneeraj2002@gmail.com>
2023-08-21 12:21:02 +0530

    Added a Banner Image to the Home

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Home.css'
+import Banner from './assets/Banner.jpg'
 
 function Home() {
   return (
     <div className='home'>
-        
+        <div className="home__container">
+            <img src={Banner} alt="Banner in Home Page" />
+        </div>
     </div>
   )
 }
